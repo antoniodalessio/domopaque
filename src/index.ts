244606e41ce02c52e9234ba412e47c81f5fe33fa
Antonio D'Alessio <antonio@adias.it>
2019-11-08 13:15:43 +0100

    ...

--- src/index.ts
+++ src/index.ts
@@ -92,7 +92,7 @@ function createRoutes() {
 
   app.get('/api/actuators', function (req, res) {
     let actuators = homeController.listActuators().map( (actuator) => {
-      actuator.getData()
+      return actuator.getData()
     });
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(actuators));
